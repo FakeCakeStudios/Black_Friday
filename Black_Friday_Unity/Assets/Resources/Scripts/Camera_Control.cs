c4528d7df05f348608d578f2ea1675bd43646984
Joshua Murrill <murrills@hotmail.com>
2014-10-21 10:14:41 -0700

    Added Key Item Prefabs
    Added the script to control the cost and savings for each shopping item
    for level 1. Imported Unity particle package.

--- Black_Friday_Unity/Assets/Resources/Scripts/Camera_Control.cs
+++ Black_Friday_Unity/Assets/Resources/Scripts/Camera_Control.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class Camera_Control : MonoBehaviour
 {
-	private Vector3 offset;
-	private Transform player;
+	private Vector3 	offset;
+	private Transform 	player;
+	private bool 		active;
+
 
 	// Use this for initialization
 	void Start()
 	{
 		offset = new Vector3(0.0f, 0.0f, 0.0f);
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+		active = false;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate()
 	{
-		this.transform.position = player.position + offset;
+		if(active)
+		{
+			this.transform.position = player.position + offset;
+		}
+	}
+
+	public void SetActive(bool source)
+	{
+		active = source;
 	}
 }
---
+++ Black_Friday_Unity/Assets/Resources/Scripts/KeyItem_Control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyItem_Control : MonoBehaviour
+{
+	public int cost;
+	public int savings;
+
+	public int GetCost()
+	{
+		return cost;
+	}
+
+	public int GetSavings()
+	{
+		return savings;
+	}
+
+}
