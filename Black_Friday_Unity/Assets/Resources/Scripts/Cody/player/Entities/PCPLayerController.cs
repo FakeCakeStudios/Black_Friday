5c11e01e63f454e616622b050583459d50d19f78
cblower <codymblower@gmail.com>
2014-10-20 20:09:56 -0700

    PlayerController & CameraOverview
    Still needs some work to integrate, but It adds a folder in the scripts
    directory that I will be where I putting all my scripts.

---
+++ Black_Friday_Unity/Assets/Resources/Scripts/Cody/player/Entities/PCPLayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PCPLayerController : MonoBehaviour {
+	public SpeedsStats speeds;
+	public CameraStats cameraSet;
+
+	private CharacterController controller;
+	private Vector3 moveDirection;
+	private Vector3 rotDirection;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update(){
+		// -------------------------------------------------- //
+		// --------------- Camera Settings ------------------ //
+		// -------------------------------------------------- //
+		Camera.main.transform.localPosition = cameraSet.offset;
+		Camera.main.transform.LookAt (cameraSet.cameraObject.transform.position + cameraSet.lookOffset);
+
+		// -------------------------------------------------- //
+		// -------------- Rotation Settings ----------------- //
+		// -------------------------------------------------- //
+		rotDirection = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+		rotDirection *= speeds.mouseSensitivity;
+
+		// -------------------------------------------------- //
+		// --------------- Moving Settings ------------------ //
+		// -------------------------------------------------- //
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection *= speeds.speed;
+		moveDirection = transform.TransformDirection(moveDirection);
+
+		moveDirection.y -= speeds.gravity * Time.deltaTime;
+
+		// -------------------------------------------------- //
+		// ---------- Actually Moving/Rotating -------------- //
+		// -------------------------------------------------- //
+		transform.Rotate(new Vector3(0, rotDirection.y, 0));
+		cameraSet.cameraObject.transform.Rotate(new Vector3(rotDirection.x, 0, 0));
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
---
+++ Black_Friday_Unity/Assets/Resources/Scripts/Cody/player/Entities/PlayerController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : EntityCharacter{
+	//------------------------------------------------------------------------------------//
+	//--------------- Class that can be overrided for NPCs and Guards --------------------//
+	//------------------------------------------------------------------------------------//
+}
