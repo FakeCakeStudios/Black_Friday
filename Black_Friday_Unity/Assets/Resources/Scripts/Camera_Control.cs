7f507893dc57cdba76af63f21fb1b89f34c32602
Joshua Murrill <murrills@hotmail.com>
2014-10-21 08:59:12 -0700

    Heirarchy Organized
    Added a camera script for offsetting from the player. Didn't see one in
    your folders Cody

---
+++ Black_Friday_Unity/Assets/Resources/Scripts/Camera_Control.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Control : MonoBehaviour
+{
+	private Vector3 offset;
+	private Transform player;
+
+	// Use this for initialization
+	void Start()
+	{
+		offset = new Vector3(0.0f, 0.0f, 0.0f);
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		this.transform.position = player.position + offset;
+	}
+}
