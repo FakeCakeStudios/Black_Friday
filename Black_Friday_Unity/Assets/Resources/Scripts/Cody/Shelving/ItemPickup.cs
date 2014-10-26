321bf75d96a3159ecd1cf40e9f7d636a35045204
Joshua Murrill <murrills@hotmail.com>
2014-10-26 10:19:46 -0700

    Corrections and Optimizing
    Added Cody's Shelving folder back to project and removed some uneeded
    assets in the project, such as Unity's Controller folder with
    constructor

---
+++ Black_Friday_Unity/Assets/Resources/Scripts/Cody/Shelving/ItemPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickup : MonoBehaviour {
+	public GameObject itemSpawner;
+	public float pickUpDistance = 1f;
+	private _ItemSpawner spawner;
+	//private ScoreBoard score;
+	void Awake(){
+		spawner = itemSpawner.GetComponent<_ItemSpawner>();
+		//score = GetComponent<ScoreBoard> ();
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "item"){
+			//ScoreBoard.score += other.gameObject.GetComponent<ItemPickupItem>().thisItem.score;
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Black_Friday_Unity/Assets/Resources/Scripts/Cody/Shelving/ItemPickupItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickupItem : MonoBehaviour {
+	public float rotationSpeed = 5f;
+	public itemStuff[] items;
+	public itemStuff thisItem;
+
+	void Start(){
+		int temp = Mathf.RoundToInt (Random.Range (0, items.Length));
+		GameObject clone = Instantiate(items[temp].mesh, transform.position, Quaternion.identity) as GameObject;
+		thisItem = items[temp];
+		clone.transform.parent = transform;
+	}
+	void Update(){
+		transform.Rotate(0, rotationSpeed*Time.deltaTime, 0, Space.World);
+
+	}
+}
+[System.Serializable]
+public class itemStuff{
+	public GameObject mesh;
+	public int score;
+}
